e2dacf6d253b57d671e56c2c03bbe5bd9228f571
seenger-sir <90158346+seenger-sir@users.noreply.github.com>
2021-09-05 20:15:41 -0300

    Create yt_downloader.py

---
+++ yt_downloader.py
@@ -0,0 +1,22 @@
+import pytube; import os; import shutil; from moviepy.editor import *
+
+url = str(input('Digite a URL do vídeo: '))
+yt = pytube.YouTube(url)
+formato = str(input('Deseja baixar o vídeo no formato MP3 ou MP4?: ')).upper()
+
+if formato == 'MP4':
+  video = yt.streams.get_highest_resolution()
+  download = video.download()
+  print(f'Vídeo "{yt.title}" baixado!')
+
+if formato == 'MP3':
+  video = yt.streams.get_highest_resolution()
+  download = video.download()
+  clip = VideoFileClip(download)
+  audio = clip.audio
+  audio.write_audiofile(download + '.mp3')
+  path = os.path.abspath(download)
+  diretorio = os.listdir(path)
+  for file in diretorio:
+      if file == f'{download}':
+          os.remove(file)
