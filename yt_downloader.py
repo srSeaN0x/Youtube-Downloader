cbe98d4ef9d77189e607c4200584512f9bdb4585
seenger-sir <90158346+seenger-sir@users.noreply.github.com>
2021-09-08 13:34:55 -0300

    Create yt_downloader.py

---
+++ yt_downloader.py
@@ -0,0 +1,31 @@
+import pytube; import os; from moviepy.editor import *; from colorama import Fore, init
+
+init(autoreset = True)
+
+try:
+  os.system('clear') # caso seu OS não seja windows e tenha dado erro mude para "cls" 
+  url = str(input(Fore.MAGENTA + 'Digite a URL do vídeo: '))
+  yt = pytube.YouTube(url)
+  formato = str(input(Fore.MAGENTA + 'Deseja baixar o vídeo no formato MP3 ou MP4?: ')).upper()
+
+  if formato == 'MP4':
+    video = yt.streams.get_highest_resolution()
+    download = video.download()
+    print(f'\nVídeo "{yt.title}" baixado!')
+
+  if formato == 'MP3':
+    video = yt.streams.get_highest_resolution()
+    download = video.download()
+    join = os.path.join(download)
+    clip = VideoFileClip(join)
+    audio = clip.audio
+    nome_novo = audio.write_audiofile(join + '.mp3')
+    nome_arquivo = f'''{yt.title.replace("'", '')}.mp4'''
+    os.remove(nome_arquivo)
+    os.system('clear') # mesma coisa do primeiro comentário
+    msg_sucesso = f'Áudio do vídeo "{yt.title}" baixado com sucesso.'
+    print(Fore.BLACK + msg_sucesso)
+
+except TypeError as erro:
+  msg_erro = f'Houve um erro. Tente novamente. (Erro: {erro})'
+  print(Fore.RED + msg_erro)
