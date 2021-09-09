3fd55d73b7c4aa21e6fdddcc4046c83fd211c707
Ares <90158346+MonsieurAres@users.noreply.github.com>
2021-09-09 09:50:29 -0300

    Update yt_downloader.py

--- yt_downloader.py
+++ yt_downloader.py
@@ -3,7 +3,7 @@ import pytube; import os; from moviepy.editor import *; from colorama import For
 init(autoreset = True)
 
 try:
-  os.system('clear') # caso seu OS não seja windows e tenha dado erro mude para "cls" 
+  os.system('cls') # caso seu OS não seja windows e tenha dado erro mude para "clear" 
   url = str(input(Fore.MAGENTA + 'Digite a URL do vídeo: '))
   yt = pytube.YouTube(url)
   formato = str(input(Fore.MAGENTA + 'Deseja baixar o vídeo no formato MP3 ou MP4?: ')).upper()
@@ -22,7 +22,7 @@ try:
     nome_novo = audio.write_audiofile(join + '.mp3')
     nome_arquivo = f'''{yt.title.replace("'", '')}.mp4'''
     os.remove(nome_arquivo)
-    os.system('clear') # mesma coisa do primeiro comentário
+    os.system('cls') # mesma coisa do primeiro comentário
     msg_sucesso = f'Áudio do vídeo "{yt.title}" baixado com sucesso.'
     print(Fore.BLACK + msg_sucesso)
 
