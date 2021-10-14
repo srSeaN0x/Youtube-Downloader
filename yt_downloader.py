8c24863f61360bcdd26bb521cb0a679bffec3f87
sieger <90158346+s1eger@users.noreply.github.com>
2021-10-14 16:05:48 -0300

    Update yt_downloader.py

--- yt_downloader.py
+++ yt_downloader.py
@@ -2,32 +2,51 @@ import pytube
 import os
 from moviepy.editor import *
 from colorama import Fore, init
+from time import sleep
 
 init(autoreset = True)
 
 try:
-  os.system('clear' if os.name != 'nt' else 'cls') 
-  url = str(input(Fore.MAGENTA + 'Digite a URL do vídeo: '))
-  yt = pytube.YouTube(url)
-  formato = str(input(Fore.MAGENTA + 'Deseja baixar o vídeo no formato MP3 ou MP4?: ')).strip().upper()
+  while True:
+    def clear():
+      os.system('clear' if os.name != 'nt' else 'cls') 
+    
 
-  if formato == 'MP4':
-    video = yt.streams.get_highest_resolution()
-    download = video.download()
-    print(f'\nVídeo "{yt.title}" baixado!')
+    clear()
+    url = str(input(Fore.MAGENTA + 'Digite a URL do vídeo: '))
+    yt = pytube.YouTube(url)
+    formato = str(input(Fore.MAGENTA + 'Deseja baixar o vídeo no formato MP3 ou MP4?: ')).strip().upper()
 
-  if formato == 'MP3':
-    video = yt.streams.get_highest_resolution()
-    download = video.download()
-    join = os.path.join(download)
-    clip = VideoFileClip(join)
-    audio = clip.audio
-    nome_novo = audio.write_audiofile(str(join).replace('mp4', 'mp3'))
-    nome_arquivo = f'''{yt.title.replace("." if '.' in yt.title else "'", '')}.mp4'''
-    os.remove(nome_arquivo)
-    os.system('clear' if os.name != 'nt' else 'cls') 
-    msg_sucesso = f'Áudio do vídeo "{yt.title}" baixado com sucesso.'
-    print(Fore.BLACK + msg_sucesso)
+    if formato == 'MP4':
+      video = yt.streams.get_highest_resolution()
+      download = video.download()
+      print(f'\nVídeo "{yt.title}" baixado!')
+      sleep(3.3)
+      clear()
+      continue
+
+    if formato == 'MP3':
+      video = yt.streams.get_highest_resolution()
+      download = video.download()
+      join = os.path.join(download)
+      clip = VideoFileClip(join)
+      audio = clip.audio
+      nome_novo = audio.write_audiofile(str(join).replace('mp4', 'mp3'))
+      nome_arquivo = f'''{yt.title.replace("." if '.' in yt.title else "'", '')}.mp4'''
+      os.remove(nome_arquivo)
+      os.system('clear' if os.name != 'nt' else 'cls') 
+      msg_sucesso = f'Áudio do vídeo "{yt.title}" baixado com sucesso.'
+      print(Fore.BLACK + msg_sucesso)
+      sleep(3.3)
+      clear()
+      continue
+
+    elif formato not in ('MP4', 'MP3'):
+      MSG_ERRO = f'Você digitou algo errado, ou o programa não tem opções para esse formato.'
+      print(Fore.RED + MSG_ERRO)
+      sleep(4.4)
+      clear()
+      continue
 
 except Exception as error:
   msg_erro = f'Houve um erro :(.'
